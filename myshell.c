2731dad34fa5947d156dff6d7922ca620e860688
ChanderG <chandergovind@gmail.com>
2015-01-26 16:30:43 +0530

    Refactors code with simple command handler

--- myshell.c
+++ myshell.c
@@ -25,6 +25,42 @@ void getPrompt(char prompt[]){
   return;
 }
 
+//handle clean up after a command
+void postCommandHandler(char* input){
+  //store input to history
+
+  //free space
+  free(input);
+  return;
+}  
+
+//function to be called to "exit" the shell
+void preExit(char* input){
+  //execute normal command completion routine
+  postCommandHandler(input);
+  //game over
+  exit(1);
+}
+
+//parse the input and call the corresponding function
+void handleCommand(char* input){
+  //buffer the same size as input, for safety 
+  char* command = (char*)malloc(sizeof(input));
+  sscanf(input, "%s ", command);
+
+  if(strcmp(command,"exit") == 0){
+    free(command);
+    preExit(input);
+  }
+
+  //understand the command and take relevant action
+  puts(input); 
+  puts(command); 
+
+  free(command);
+  return;
+}  
+
 int main(int argc, char** argv){
   
   char* input;                        //to store input
@@ -36,8 +72,12 @@ int main(int argc, char** argv){
     getPrompt(prompt);
 
     input = readline(prompt);
-    puts("Input accepted.");
-    if(strcmp(input,"exit") == 0) exit(1);
+    
+    //main routine 
+    handleCommand(input); 
+
+    //handle clean up
+    postCommandHandler(input);
   }
 
   return 0;
