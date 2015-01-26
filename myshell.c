abc8ad2250fab4383dab410f2ad067e9af93d3e2
ChanderG <chandergovind@gmail.com>
2015-01-26 16:09:27 +0530

    Refactors code, adds error handling

--- myshell.c
+++ myshell.c
@@ -3,26 +3,34 @@
 //An attempt at a shell
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 #include<limits.h>
 #include<string.h>
 #include<readline/readline.h>
 #include<readline/history.h>
+#include<error.h>
 
 //needed constants
+ //mem size for prompt string
 #define PROMPT_SIZE PATH_MAX + 2
 
+//calculate the prompt string using current directory
 void getPrompt(char prompt[]){
-  getcwd(prompt, PROMPT_SIZE);  
+  if(getcwd(prompt, PROMPT_SIZE) == NULL){
+    perror("Failed to get cwd path.");
+    strcpy(prompt, "path/not/found");
+  }
+  //add decorations
   strcat(prompt, "> "); 
   return;
 }
 
 int main(int argc, char** argv){
   
-  //main loop
   char* input;                        //to store input
   char prompt[PROMPT_SIZE];           //prompt to be displayed to user
 
+  //main loop
   while(1){
     //calculate prompt
     getPrompt(prompt);
@@ -31,5 +39,6 @@ int main(int argc, char** argv){
     puts("Input accepted.");
     if(strcmp(input,"exit") == 0) exit(1);
   }
+
   return 0;
 }
